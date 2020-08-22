36ca5a94b23a69ec1b0e66f13a5283043ce2f3aa
Lukas <niederbergerlukas@gmail.com>
2020-08-21 17:04:57 -0700

    Normalize headers to lowercase

--- lib/ex_esi/request/hackney.ex
+++ lib/ex_esi/request/hackney.ex
@@ -1,19 +1,20 @@
 defmodule ExEsi.Request.Hackney do
-  @behaviour ExEsi.Request.HttpClient
+  alias ExEsi.Request.HttpClient
+  @behaviour HttpClient
 
   @default_opts [recv_timeout: 30_000]
 
-  @impl ExEsi.Request.HttpClient
+  @impl HttpClient
   def request(method, url, body \\ "", headers \\ [], http_opts \\ []) do
     opts = Application.get_env(:ex_esi, :hackney_ops, @default_opts)
     opts = http_opts ++ [:with_body | opts]
 
     case :hackney.request(method, url, headers, body, opts) do
       {:ok, status, headers} ->
-        {:ok, %{status_code: status, headers: headers}}
+        {:ok, %{status_code: status, headers: HttpClient.normalize_headers(headers)}}
 
       {:ok, status, headers, body} ->
-        {:ok, %{status_code: status, headers: headers, body: body}}
+        {:ok, %{status_code: status, headers: HttpClient.normalize_headers(headers), body: body}}
 
       {:error, reason} ->
         {:error, %{reason: reason}}
