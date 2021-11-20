44b57c9d25ca501dcdd8a9a735b4c92001a77bc1
Lukas Niederberger <niederbergerlukas@gmail.com>
2021-11-19 21:02:20 -0800

    Update Route params to actually work

--- lib/ex_esi/api/routes.ex
+++ lib/ex_esi/api/routes.ex
@@ -24,25 +24,32 @@ defmodule ExEsi.API.Routes do
   @spec route(integer, integer, security_preference, list(integer), list(Connection.t())) ::
           ExEsi.Operation.JSON.t()
   def route(origin, destination, flag \\ :shortest, avoid \\ [], connections \\ []) do
-    params = "flag=#{flag}"
-
     params =
-      case avoid do
-        [] -> params
-        list when is_list(list) -> "&avoid=" <> Enum.join(list, ",")
-      end
-
-    params =
-      case connections do
-        [] ->
-          params
-
-        list when is_list(list) ->
-          "&connections=" <> (Enum.map(connections, &format_connection/1) |> Enum.join(","))
-      end
+      %{}
+      |> put_flag(flag)
+      |> put_avoid(avoid)
+      |> put_connections(connections)
 
     "/#{@version}/route/#{origin}/#{destination}/"
     |> API.get("", params)
+  end
+
+  defp put_flag(params, flag) do
+    Map.put(params, "flag", to_string(flag))
+  end
+
+  defp put_avoid(params, []), do: params
+  defp put_avoid(params, avoid) do
+    Map.put(params, "avoid", Enum.join(avoid, ","))
+  end
+
+  defp put_connections(params, []), do: params
+  defp put_connections(params, connections) do
+    Map.put(
+      params,
+      "connections",
+      Enum.map(connections, &format_connection/1) |> Enum.join(",")
+    )
   end
 
   defp format_connection(%Connection{bidirectional: true} = conn) do
